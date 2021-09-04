68b33f67ad1ba55338890cba82a76f1c5391645c
PTER <truakka@gmail.com>
2021-09-04 10:39:41 +0200

    Changes

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,4 +1,22 @@
 
 public class ComplexNumber {
 
+	double realPart;
+	double imaginaryPart;
+	private double getRealPart() {
+		return realPart;
+	}
+	private void setRealPart(double realPart) {
+		this.realPart = realPart;
+	}
+	private double getImaginaryPart() {
+		return imaginaryPart;
+	}
+	private void setImaginaryPart(double imaginaryPart) {
+		this.imaginaryPart = imaginaryPart;
+	}
+	
+	
+	
+	
 }
